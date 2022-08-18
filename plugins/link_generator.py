bec7c0cd09eb95b36a57bd5884259c1b6110a732
GOVINDA888 <105217504+GOVINDA888@users.noreply.github.com>
2022-08-18 17:26:11 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -17,26 +17,26 @@ async def batch(client: Client, message: Message):
         if f_msg_id:
             break
         else:
-            await first_message.reply("<b>🚫 𝙴𝚁𝚁𝙾𝚁\n\n𝚃𝙷𝙸𝚂 𝙵𝙾𝚁𝚆𝙰𝚁𝙳𝙴𝙳 𝙿𝙾𝚂𝚃 𝙸𝚂 𝙽𝙾𝚃 𝙵𝚁𝙾𝙼 𝙼𝚈 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝙾𝚁 𝙻𝙸𝙽𝙺 𝙸𝚂 𝙽𝙾𝚃 𝚃𝙰𝙺𝙴𝙽 𝙵𝚁𝙾𝙼 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻...</b>", quote = True)
+            await first_message.reply("<b>🚫 𝙴𝚁𝚁𝙾𝚁\n\n𝚃𝙷𝙸𝚂 𝙵𝙾𝚁𝚆𝙰𝚁𝙳𝙴𝙳 𝙿𝙾𝚂𝚃 𝙸𝚂 𝙽𝙾𝚃 𝙵𝚁𝙾𝙼 𝙼𝚈 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝙾𝚁 𝙻𝙸𝙽𝙺 𝙸𝚂 𝙽𝙾𝚃 𝚃𝙰𝙺𝙴𝙽 𝙵𝚁𝙾𝙼 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 🚫</b>", quote = True)
             continue
 
     while True:
         try:
-            second_message = await client.ask(text = "<b>𝙵𝙾𝚁𝚆𝙰𝚁𝙳 𝚃𝙷𝙴 𝙻𝙰𝚂𝚃 𝙼𝙴𝚂𝚂𝙰𝙶𝙴 𝙵𝚁𝙾𝙼 𝚃𝙷𝙴 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 (𝚆𝙸𝚃𝙷 𝙵𝙾𝚁𝚆𝙰𝚁𝙳 𝚀𝙾𝚄𝚃𝙴)....\n\n𝙾𝚁\n\n𝚂𝙴𝙽𝙳 𝚃𝙷𝙴 𝙿𝙾𝚂𝚃 𝙻𝙸𝙽𝙺</b>", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            second_message = await client.ask(text = "<b>😉 𝙵𝙾𝚁𝚆𝙰𝚁𝙳 𝚃𝙷𝙴 𝙻𝙰𝚂𝚃 𝙼𝙴𝚂𝚂𝙰𝙶𝙴 𝙵𝚁𝙾𝙼 𝚃𝙷𝙴 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 (𝚆𝙸𝚃𝙷 𝙵𝙾𝚁𝚆𝙰𝚁𝙳 𝚀𝙾𝚄𝚃𝙴) 😉\n\n𝙾𝚁\n\n𝚂𝙴𝙽𝙳 𝚃𝙷𝙴 𝙿𝙾𝚂𝚃 𝙻𝙸𝙽𝙺</b>", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         s_msg_id = await get_message_id(client, second_message)
         if s_msg_id:
             break
         else:
-            await second_message.reply("<b>🚫 𝙴𝚛𝚛𝚘𝚛\n\n𝚃𝙷𝙸𝚂 𝙵𝙾𝚁𝚆𝙰𝚁𝙳𝙴𝙳 𝙿𝙾𝚂𝚃 𝙸𝚂 𝙽𝙾𝚃 𝙵𝚁𝙾𝙼 𝙼𝚈 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝙾𝚁 𝙻𝙸𝙽𝙺 𝙸𝚂 𝙽𝙾𝚃 𝚃𝙰𝙺𝙴𝙽 𝙵𝚁𝙾𝙼 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻...</b>", quote = True)
+            await second_message.reply("<b>🚫 𝙴𝚛𝚛𝚘𝚛\n\n✋ 𝚃𝙷𝙸𝚂 𝙵𝙾𝚁𝚆𝙰𝚁𝙳𝙴𝙳 𝙿𝙾𝚂𝚃 𝙸𝚂 𝙽𝙾𝚃 𝙵𝚁𝙾𝙼 𝙼𝚈 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝙾𝚁 𝙻𝙸𝙽𝙺 𝙸𝚂 𝙽𝙾𝚃 𝚃𝙰𝙺𝙴𝙽 𝙵𝚁𝙾𝙼 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 🤩</b>", quote = True)
             continue
 
 
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝙷𝙰𝚁𝙴 𝚄𝚁𝙻", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🧡 𝚂𝙷𝙰𝚁𝙴 𝚄𝚁𝙻 🧡", url=f'https://telegram.me/share/url?url={link}')]])
     await second_message.reply_text(f"<b>𝙷𝙴𝚁𝙴 𝙸𝚂 𝚈𝙾𝚄𝚁 𝙻𝙸𝙽𝙺</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
@@ -51,7 +51,7 @@ async def link_generator(client: Client, message: Message):
         if msg_id:
             break
         else:
-            await channel_message.reply("<b>🚫 𝙴𝚛𝚛𝚘𝚛\n\n𝚃𝙷𝙸𝚂 𝙵𝙾𝚁𝚆𝙰𝚁𝙳𝙴𝙳 𝙿𝙾𝚂𝚃 𝙸𝚂 𝙽𝙾𝚃 𝙵𝚁𝙾𝙼 𝙼𝚈 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝙾𝚁 𝙻𝙸𝙽𝙺 𝙸𝚂 𝙽𝙾𝚃 𝚃𝙰𝙺𝙴𝙽 𝙵𝚁𝙾𝙼 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻...</b>", quote = True)
+            await channel_message.reply("<b>🚫 𝙴𝚛𝚛𝚘𝚛\n\n𝚃𝙷𝙸𝚂 𝙵𝙾𝚁𝚆𝙰𝚁𝙳𝙴𝙳 𝙿𝙾𝚂𝚃 𝙸𝚂 𝙽𝙾𝚃 𝙵𝚁𝙾𝙼 𝙼𝚈 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝙾𝚁 𝙻𝙸𝙽𝙺 𝙸𝚂 𝙽𝙾𝚃 𝚃𝙰𝙺𝙴𝙽 𝙵𝚁𝙾𝙼 𝙳𝙱 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ✋.</b>", quote = True)
             continue
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
